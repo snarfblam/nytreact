5115c8c9af563c27fcea7bbb9d4ff7c48565f2c1
Tom H <snarfblam@gmail.com>
2018-05-05 20:42:53 -0400

    Added search panel

---
+++ client/src/components/SearchPanel/SearchPanel.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Card, CardBody, CardHead } from '../Card';
+import { Form, Input, Select } from '../Form';
+// import * as API from '../../NytApi';
+
+class SearchPanel extends React.Component {
+    constructor(props) {
+        super(props); 
+
+        this.state = {
+            searchTerm: "",
+            numRecords: 5,
+            startYear: "",
+            endYear: "",
+        }
+
+        this.handleChange = (event) => {
+            this.setState({
+                [event.target.name]: event.target.value
+            });
+        };
+
+        this.onSubmit = (event) => {
+            event.preventDefault();
+            // API.searchArticles(this.state.searchTerm, this.state.numRecords, this.state.startYear, this.state.endYear)
+            //     .then(result => {
+            //         console.log(result);
+            //     });
+            this.props.onSearch(this.state);
+        }
+    }
+
+
+    render() {
+        return (
+            <Card>
+                <CardHead>Search NY Times</CardHead>
+                <CardBody>
+                    <Form>
+                        <Input
+                            name='searchTerm'
+                            label='Search Term:'
+                            onChange={this.handleChange}
+                            value={this.state.searchTerm}
+                        />
+                        <Select
+                            name='numRecords'
+                            label='Number of Records to Retrieve:'
+                            onChange={this.handleChange}
+                            value={this.state.numRecords} >
+                            <option>1</option>    
+                            <option>5</option>    
+                            <option>10</option>    
+                        </Select>    
+                        <Input
+                            name='startYear'
+                            label='Start Year (optional):'
+                            onChange={this.handleChange}
+                            value={this.state.startYear}
+                        />
+                        <Input
+                            name='endYear'
+                            label='End Year (optional):'
+                            onChange={this.handleChange}
+                            value={this.state.endYear}
+                        />
+                        <button className='btn btn-primary' type='submit' onClick={this.onSubmit}>Search</button>
+                    </Form>
+                </CardBody>
+            </Card>
+        );    
+    }
+}
+
+export default SearchPanel;
